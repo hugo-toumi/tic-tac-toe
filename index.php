4f41e1efbcf9094afa7a40cdc9c9ac17ae9c125d
yanis-khiter <yanis.khiter@laplateforme.io>
2022-01-27 16:39:54 +0100

    changement dans l'index + le style + image + polic ;;;  jeu / ia / connect pas touché

--- index.php
+++ index.php
@@ -1,32 +1,43 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Page d'accueil</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Page d'accueil</title>
 
 </head>
-<body class ="bodyindex">
 
-<div class="loader">
-			<span>T</span>
-			<span>I</span>
-			<span>C</span>
-			<span>T</span>
-			<span>A</span>
-			<span>C</span>
-			<span>T</span>
-            <span>O</span>
-            <span>E</span>
-			
+<body class="bodyindex">
 
-		</div>
+	<header>
 
+		<img src="techo.jpg" alt="">
 
-    
+		<nav id="navhead">
+
+			<a href="jeu.php">Jeu</a>
+
+			<a href="inscription.php">Inscription</a>
+
+			<a href="connexion.php"></i>Connexion</a>
+
+			<a href="profil.php"></i>Profil</a>
+
+		</nav>
+
+	</header>
+
+	<h1 class="h1index">TIC TAC TOE</h1>
+
+	<h3 class="h3index">Prêt à défier notre IA Robingo ?</h3>
+
+	<h2 class="h2play">PLAY </h2>
+
+	<a class="play-btn" href="jeu.php"></a>
+
 </body>
+
 </html>
